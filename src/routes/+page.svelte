317f81517f89fd00542438214cdcca788cacfa3d
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-11-03 20:42:56 +0100

    extract Table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,61 +1,21 @@
 <script lang="ts">
-	import { dataSources } from '$lib/constants/dataSources';
 	import type { PageData } from './$types';
-
-	const sources = Object.values(dataSources);
+	import Table from './Table.svelte';
 
 	let { data }: { data: PageData } = $props();
 </script>
 
-<table class="table">
-	<thead>
-		<tr>
-			<th>Rank</th>
-			<th>Country</th>
-			{#each sources as source}
-				<th>
-					<a href={source.url} target="_blank" rel="noreferrer">
-						{source.name}
-					</a>
-				</th>
-			{/each}
-		</tr>
-	</thead>
-	<tbody>
-		{#each data.countries as country (country.code)}
-			<tr>
-				<td>{country.average}</td>
-				<td>{country.name}</td>
-				{#each sources as source (source.code)}
-					<td>
-						{country.rankings?.[source.code]}
-					</td>
-				{/each}
-			</tr>
-		{/each}
-	</tbody>
-</table>
+<main>
+	<Table countries={data.countries} />
+</main>
 
 <style>
-	.table {
-		background-color: #f8f9fa;
-		color: #202122;
-		margin: 1em 0;
-		border: 1px solid #a2a9b1;
-		border-collapse: collapse;
-		text-align: center;
-		font-size: 100%;
-	}
-
-	.table thead tr > th {
-		background-color: #eaecf0;
-		text-align: center;
-		border: 1px solid #a2a9b1;
-		padding: 0.2em 0.4em;
-	}
-
-	.table tr > th {
-		border: 1px solid #a2a9b1;
-		padding: 0.2em 0.4em;
+	main {
+		display: flex;
+		flex-direction: column;
+		justify-content: space-between;
+		align-items: center;
+		padding: 6rem;
+		min-height: 100vh;
 	}
 </style>
---
+++ src/routes/Table.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import { dataSources } from '$lib/constants/dataSources';
+	import type { PreparedCountry } from '$lib/schema/country';
+
+	const sources = Object.values(dataSources);
+
+	let props: { countries: PreparedCountry[] } = $props();
+</script>
+
+<table>
+	<thead>
+		<tr>
+			<th>Rank</th>
+			<th>Country</th>
+			{#each sources as source}
+				<th>
+					<a href={source.url} target="_blank" rel="noreferrer">
+						{source.name}
+					</a>
+				</th>
+			{/each}
+		</tr>
+	</thead>
+	<tbody>
+		{#each props.countries as country (country.code)}
+			<tr>
+				<td>{country.average}</td>
+				<td>{country.name}</td>
+				{#each sources as source (source.code)}
+					<td>
+						{country.rankings?.[source.code]}
+					</td>
+				{/each}
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style>
+	table {
+		background-color: #f8f9fa;
+		color: #202122;
+		margin: 1em 0;
+		border: 1px solid #a2a9b1;
+		border-collapse: collapse;
+		text-align: center;
+		font-size: 100%;
+	}
+
+	table thead tr > th {
+		background-color: #eaecf0;
+		text-align: center;
+		border: 1px solid #a2a9b1;
+		padding: 0.2em 0.4em;
+	}
+
+	table tr > th {
+		border: 1px solid #a2a9b1;
+		padding: 0.2em 0.4em;
+	}
+</style>
