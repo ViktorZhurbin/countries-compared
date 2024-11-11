1005bbc691e5026da74f7e1d8400ad4dca4e21d3
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-11-11 23:19:15 +0100

    add hidden table

---
+++ src/lib/components/ButtonIcon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { Snippet } from "svelte";
+
+  let { children, ...restProps }: { children: Snippet; onclick: () => void } =
+    $props();
+</script>
+
+<button class="outline" {...restProps}>
+  {@render children()}
+</button>
+
+<style>
+  button {
+    border: unset;
+    padding: 3px 6px;
+  }
+</style>
--- src/routes/tabled/+page.server.ts
+++ src/routes/tabled/+page.server.ts
@@ -1,24 +1,18 @@
 import mongoose from "mongoose";
 import { MONGODB_URI } from "$env/static/private";
 import { getCountries } from "$lib/server/database/getCountries";
-import { getLastUpdated } from "$lib/server/database/getLastUpdated";
-import geoJsonEurope from "$lib/server/database/europeGeoData.json"; // see static/data/sources.md
 import type { PageServerLoad } from "./$types";
 import type { PreparedCountry } from "$lib/schema/country";
 
 export const prerender = true;
 
 export const load: PageServerLoad<{
-  geoJsonEurope: unknown;
   countries: PreparedCountry[];
-  lastUpdated: string;
 }> = async () => {
   await mongoose.connect(MONGODB_URI);
   // mongoose.set("debug", true);
 
   return {
-    geoJsonEurope: geoJsonEurope as unknown,
     countries: JSON.parse(await getCountries()),
-    lastUpdated: JSON.parse(await getLastUpdated()),
   };
 };
