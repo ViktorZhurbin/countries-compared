67ae20a682962678c1a1fe5c5519a98f3818066f
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-11-11 16:20:46 +0100

    break down Map component

--- src/lib/components/Map/Map.svelte
+++ src/lib/components/Map/Map.svelte
@@ -3,27 +3,22 @@
   import { init, use, registerMap, type EChartsType } from "echarts/core";
   import type { PreparedCountry } from "$lib/schema/country";
   import { StaticDataSourceId } from "$lib/constants/dataSources/static";
-  import DataSourcePicker from "../DataSourcePicker/DataSourcePicker.svelte";
-  import UpdateButton from "../UpdateButton/UpdateButton.svelte";
   import { getOptions } from "./helpers/getOptions";
   import { chartModules } from "./constants/chartModules";
 
   let props: {
     geoJson: any;
-    lastUpdated: string;
+    dataId: StaticDataSourceId;
     countries: PreparedCountry[];
   } = $props();
 
   const MAP_ID = "Europe";
 
-  let countries = $state(props.countries);
-  let dataId = $state(StaticDataSourceId.HDI);
-
   let options = $derived(
     getOptions({
-      dataId,
-      countries,
       mapId: MAP_ID,
+      dataId: props.dataId,
+      countries: props.countries,
     }),
   );
 
@@ -44,33 +39,11 @@
   $effect(() => {
     chart.setOption(options);
   });
-
-  const setDataId = (newDataId: StaticDataSourceId) => {
-    dataId = newDataId;
-  };
-
-  const setCountries = (newCountries: PreparedCountry[]) => {
-    countries = newCountries;
-  };
 </script>
 
-<div class="wrapper">
-  <DataSourcePicker {dataId} {setDataId} />
-
-  <div class="chart" bind:this={chartEl}></div>
-
-  <UpdateButton {setCountries} lastUpdated={props.lastUpdated} />
-</div>
+<div class="chart" bind:this={chartEl}></div>
 
 <style>
-  .wrapper {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 16px;
-  }
-
   .chart {
     height: 70dvh;
     width: 90dvw;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,32 @@
 <script lang="ts">
+  import DataSourcePicker from "$lib/components/DataSourcePicker/DataSourcePicker.svelte";
   import Map from "$lib/components/Map/Map.svelte";
+  import UpdateButton from "$lib/components/UpdateButton/UpdateButton.svelte";
+  import { StaticDataSourceId } from "$lib/constants/dataSources/static";
+  import type { PreparedCountry } from "$lib/schema/country";
   import type { PageData } from "./$types";
 
   let { data }: { data: PageData } = $props();
+
+  let countries = $state(data.countries);
+  let dataId = $state(StaticDataSourceId.HDI);
 </script>
 
 <main>
-  <Map
-    countries={data.countries}
-    geoJson={data.geoJsonEurope}
+  <DataSourcePicker
+    {dataId}
+    setDataId={(newId) => {
+      dataId = newId;
+    }}
+  />
+
+  <Map {dataId} {countries} geoJson={data.geoJsonEurope} />
+
+  <UpdateButton
     lastUpdated={data.lastUpdated}
+    setCountries={(newCountries: PreparedCountry[]) => {
+      countries = newCountries;
+    }}
   />
 </main>
 
@@ -17,7 +34,8 @@
   main {
     display: flex;
     flex-direction: column;
+    justify-content: center;
     align-items: center;
-    gap: 24px;
+    gap: 16px;
   }
 </style>
